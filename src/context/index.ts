85d9f1b7d03a0f13274f2bb7121fcdbbd3fb81fd
二货机器人 <smith3816@gmail.com>
2024-07-11 14:25:05 +0800

    refactor: dependency inversion

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,5 +1,3 @@
-export { useMergedConfigContext } from './config-provider';
 export { useMergedThemeContext } from './theme-provider';
 
-export type { GetConfigProviderContext } from './config-provider';
 export type { GetThemeProviderContext } from './theme-provider';
